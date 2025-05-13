2c6479cb57d3ce715874f6d6165d205210f47b30
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-05-13 13:00:59 +0200

    Bubbles

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let red = document.getElementById("red-bubble");
+let orange = document.getElementById("orange-bubble");
 
 document.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById("krop-video");
@@ -12,11 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
       const scrollPercent = scrollPosition / pageHeight;
 
       if (scrollPercent >= 0.4) {
-        red.style.transform = `translateY(-3000px)`;
+        red.style.transform = `translateY(700px) translateX(300px)`;
       }
   
       if (scrollPercent >= 0.4 && !hasPlayed) {
         video.style.transform = `scale(${1.05})`;
+      }
+
+      if (scrollPercent >= 0.5) {
+        orange.style.transform = `translateY(700px) translateX(-700px)`;
       }
       
       if (scrollPercent >= 0.6 && !hasPlayed) {
@@ -25,8 +30,20 @@ document.addEventListener("DOMContentLoaded", () => {
         hasPlayed = true;
       }
 
+      if (scrollPercent >= 0.7) {
+        red.style.transform = `translateY(1400px) translateX(600px)`;
+      }
+
+      if (scrollPercent >= 0.7) {
+        orange.style.transform = `translateY(1000px) translateX(-300px)`;
+      }
+  
       if (scrollPercent >= 0.8) {
         video.style.transform = `scale(1.1) translateY(500px)`;
+      }
+
+      if (scrollPercent >= 0.8) {
+        orange.style.transform = `translateY(2000px) translateX(-300px)`;
       }
     });
   });
