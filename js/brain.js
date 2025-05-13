c38e45dffa6efbbfb5ddd89d865fa8d405fc1f71
Frederik Nielsen <eaa24fnie@students.eaaa.dk>
2025-05-13 17:46:16 +0200

    SVg

---
+++ js/brain.js
@@ -0,0 +1,9 @@
+"use strict"
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const blueElement = document.getElementById("st6");
+
+    blueElement.addEventListener("click", function () {
+      alert("You clicked the blue part!");
+    });
+  });
