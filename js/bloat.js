cc9ffd2d07256de53509dd453aa589b97ab9a5fe
milletoftegaard <mille@toftegaardjensen.dk>
2025-05-15 10:51:12 +0200

    bloat

--- js/bloat.js
+++ js/bloat.js
@@ -5,3 +5,18 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 1000);
   });
 
+
+  let heart = document.getElementById("heart-image");
+
+  document.addEventListener("DOMContentLoaded", () => {
+   
+      window.addEventListener("scroll", () => {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        const pageHeight = document.documentElement.scrollHeight;
+        const scrollPercent = scrollPosition / pageHeight;
+ 
+        if (scrollPercent >= 0.45) {
+         heart.style.opacity = "1";
+        } 
+      });
+    });
