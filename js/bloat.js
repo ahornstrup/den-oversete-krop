2d3b7b70f75bc18b7eb67636c17e8778ea99df1e
milletoftegaard <mille@toftegaardjensen.dk>
2025-05-15 09:51:17 +0200

    bloating

---
+++ js/bloat.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      const bloatedImg = document.getElementById('bloated-img');
+      bloatedImg.style.opacity = '1';
+    }, 1000);
+  });
+
